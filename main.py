caf3f3e62605dd62579f7f034562c7473af8e516
Matthew Guo <mg2239@cornell.edu>
2019-06-27 19:27:53 -0400

    added two color palettes

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import hashlib
 from PIL import ImageDraw, Image, ImageColor
 
-palette = ["#DDE5FF", "#9BB2FF", "#7A99FF", "#597FFF"]
+palettes = {
+    "ocean": ["#DDE5FF", "#9BB2FF", "#7A99FF", "#597FFF"],
+    "sunset": ["#FFE0BA", "#FFB978", "#FF898B", "#DB8CD2"],
+}
 icon = [
-    ["#ff0000", "#ff0000", "#ff0000", "#ff0000", "#ff0000", "#ff0000"],
-    ["#ff0000", "#ff0000", "#ff0000", "#ff0000", "#ff0000", "#ff0000"],
-    ["#ff0000", "#ff0000", "#ff0000", "#ff0000", "#ff0000", "#ff0000"],
-    ["#ff0000", "#ff0000", "#ff0000", "#ff0000", "#ff0000", "#ff0000"],
-    ["#ff0000", "#ff0000", "#ff0000", "#ff0000", "#ff0000", "#ff0000"],
-    ["#ff0000", "#ff0000", "#ff0000", "#ff0000", "#ff0000", "#ff0000"],
+    ["#ff5555", "#ff5555", "#ff5555", "#ff5555", "#ff5555", "#ff5555"],
+    ["#ff5555", "#ff5555", "#ff5555", "#ff5555", "#ff5555", "#ff5555"],
+    ["#ff5555", "#ff5555", "#ff5555", "#ff5555", "#ff5555", "#ff5555"],
+    ["#ff5555", "#ff5555", "#ff5555", "#ff5555", "#ff5555", "#ff5555"],
+    ["#ff5555", "#ff5555", "#ff5555", "#ff5555", "#ff5555", "#ff5555"],
+    ["#ff5555", "#ff5555", "#ff5555", "#ff5555", "#ff5555", "#ff5555"],
 ]
 
 
@@ -16,7 +19,10 @@ def md5_hash(str):
     return hashlib.md5(str.encode("utf-8")).hexdigest()
 
 
-def fill_icon(hash):
+def fill_icon(hash, palette):
+    """
+        Fills the icon using a hashed string and a given palette.
+    """
     for c in range(0, 18):
         pass
 
@@ -27,13 +33,17 @@ def generate_img():
 
 def main():
     u = input("String: ")
-    n = int(input("Number of colors (1-4): "))
-    u_hash = md5_hash(u)
-    print(u_hash)
-    fill_icon(u_hash)
-    for row in icon:
-        print(row)
-    generate_img()
+    p = input("Which palette? (ocean, sunset): ").lower()
+    if p in palettes:
+        n = int(input("Number of colors (1-4): "))
+        u_hash = md5_hash(u)
+        print(u_hash)
+        fill_icon(u_hash, palettes[p])
+        for row in icon:
+            print(row)
+        generate_img()
+    else:
+        raise Exception("palette not in palettes")
 
 
 if __name__ == "__main__":
