fb3b5b9b016d4f6fe2a30e8b7db5af358ec77992
Matthew Guo <mg2239@cornell.edu>
2019-06-27 20:37:59 -0400

    finished fill_icon function

--- main.py
+++ main.py
@@ -15,35 +15,53 @@ icon = [
 ]
 
 
-def md5_hash(str):
-    return hashlib.md5(str.encode("utf-8")).hexdigest()
+def md5_hash(s):
+    """Hashes string using MD5 hash function."""
+    return hashlib.md5(s.encode("utf-8")).hexdigest()
 
+def hex_to_dec(n):
+    """Converts hexadecimal number to decimal."""
+    return int(n, 16)
 
-def fill_icon(hash, palette):
+def fill_icon(hash_str, num, palette):
     """
         Fills the icon using a hashed string and a given palette.
-    """
-    for c in range(0, 18):
-        pass
+        
+        Parameters:
+        hash_str: A hashed string.
+        num: The number of colors to use.
+        palette: A list containing four hex colors.
 
+    """
+    row = 0
+    col = 0
+    for c in range(0, 18):
+        assert row < 6 and col < 3
+        color_index = hex_to_dec(hash_str[c]) % num
+        color = palette[color_index]
+        icon[row][col] = icon[row][5 - col] = color
+        if col == 2:
+            row += 1
+            col = 0
+        else:
+            col += 1
 
 def generate_img():
     pass
 
 
 def main():
-    u = input("String: ")
-    p = input("Which palette? (ocean, sunset): ").lower()
-    if p in palettes:
-        n = int(input("Number of colors (1-4): "))
-        u_hash = md5_hash(u)
-        print(u_hash)
-        fill_icon(u_hash, palettes[p])
-        for row in icon:
-            print(row)
-        generate_img()
-    else:
-        raise Exception("palette not in palettes")
+    username = input("String: ")
+    palette = input("Which palette? (ocean, sunset): ").lower()
+    assert palette in palettes
+    num_colors = int(input("Number of colors (1-4): "))
+    assert num_colors > 0 and num_colors < 5
+    user_hash = md5_hash(username)
+    print(user_hash)
+    fill_icon(user_hash, num_colors, palettes[palette])
+    for row in icon:
+        print(row)
+    generate_img()
 
 
 if __name__ == "__main__":
